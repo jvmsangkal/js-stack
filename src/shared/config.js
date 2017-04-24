39722c6fea8dfb000d6bf9c61a14a36287dc182e
John Viscel Sangkal <jvmsangkal@gmail.com>
2017-04-24 21:49:39 +0800

    initial commit

---
+++ src/shared/config.js
@@ -0,0 +1,9 @@
+// @flow
+
+export const WEB_PORT = process.env.PORT || 8000;
+export const STATIC_PATH = '/static';
+export const APP_NAME = 'Hello World!';
+
+export const WDS_PORT = 7000;
+export const APP_CONTAINER_CLASS = 'app';
+export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`;
